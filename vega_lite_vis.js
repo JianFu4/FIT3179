df0ab2ff0218290c269e594fd4cb4eee5909ca94
jong0071 <jong0071@student.monash.edu>
2023-10-13 09:06:15 +0800

    critique2

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,14 +2,15 @@ var vg_1 = "choropleth_map.vg.json";
 vegaEmbed("#map_chart", vg_1).then(function(result) {
 }).catch(console.error);
 
-var vg_2 = "scatter_plot.vg.json";
-vegaEmbed("#scatter_plot", vg_2).then(function(result) {
+var vg_2 = "pie_chart.vg.json";
+vegaEmbed("#pie_chart", vg_2).then(function(result) {
 }).catch(console.error);
+
 
 var vg_3 = "bar_chart.vg.json";
 vegaEmbed("#bar_chart", vg_3).then(function(result) {
 }).catch(console.error);
 
-var vg_4 = "test.vg.json";
+var vg_4 = "scatter_plot_test.vg.json";
 vegaEmbed("#test", vg_4).then(function(result) {
 }).catch(console.error);
