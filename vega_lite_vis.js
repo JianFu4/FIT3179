b3df583515494900d9d744dc5cba3efd344181b7
jong0071 <jong0071@student.monash.edu>
2023-10-17 21:03:01 +0800

    update

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,7 +1,19 @@
 
+var vg_1 = "symbol_map.vg.json";
+vegaEmbed("#map", vg_1).then(function(result) {
+}).catch(console.error);
+var vg_2 = "radial_chart.vg.json";
+vegaEmbed("#radial", vg_2).then(function(result) {
+}).catch(console.error);
+var vg_3 = "radar_chart1.vg.json";
+vegaEmbed("#radar1", vg_3).then(function(result) {
+}).catch(console.error);
+var vg_4 = "radar_chart2.vg.json";
+vegaEmbed("#radar2", vg_4).then(function(result) {
+}).catch(console.error);
 
-var vg_4 = "scatter_plot_test.vg.json";
-vegaEmbed("#test", vg_4).then(function(result) {
+var vg_5 = "scatter_plot_test.vg.json";
+vegaEmbed("#scatter", vg_5).then(function(result) {
 }).catch(console.error);
 
 
