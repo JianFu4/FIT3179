44cefbf458a44c67a644e595a46074f779da57ba
jong0071 <jong0071@student.monash.edu>
2023-10-17 19:08:38 +0800

    data

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,24 +1,8 @@
-var vg_1 = "choropleth_map.vg.json";
-vegaEmbed("#map_chart", vg_1).then(function(result) {
-}).catch(console.error);
 
-var vg_2 = "pie_chart.vg.json";
-vegaEmbed("#pie_chart", vg_2).then(function(result) {
-}).catch(console.error);
-
-
-var vg_3 = "bar_chart.vg.json";
-vegaEmbed("#bar_chart", vg_3).then(function(result) {
-}).catch(console.error);
 
 var vg_4 = "scatter_plot_test.vg.json";
 vegaEmbed("#test", vg_4).then(function(result) {
 }).catch(console.error);
 
-var vg_5 = "pie_test.vg.json";
-vegaEmbed("#pie_test", vg_5).then(function(result) {
-}).catch(console.error);
 
-var vg_6 = "test.vg.json";
-vegaEmbed("#test", vg_6).then(function(result) {
-}).catch(console.error);
+
